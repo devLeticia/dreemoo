cdeb118834c507e18232ade86bd64f980910a70b
Leticia <leticiagoncalves.tech@gmail.com>
2021-04-20 23:34:37 -0300

    creating nav component, inserting menu

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
 <!--adicionei a tag v-app pra encapsular toda a aplicação, assim funciona todas os atributos dentro das tags do vuetify-->
-   <v-app>
-  <div id="app">
-    <router-view/>
-  </div>
+   <v-app class="red lighten-4">
+      <nav>
+        <v-app-bar app >
+          <v-icon left light>search</v-icon>
+        </v-app-bar>
+    </nav>
+     <main-nav-bar/>
+     <router-view class="mt-16"/>
   </v-app>
 </template>
 
 
 <script>
+
+import MainNavBar from './components/navBar'
+
 export default {
-  name: 'App',
+  components: { MainNavBar },
+
 }
 </script>
 
 <style>
 #app {
   font-family: 'Nunito', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+/*   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center; */
   color: #2f2f2f;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    <h4 class="font-weight-bold">Aqui vai um titulo</h4>
-    <v-btn class="font-weight-semibold" medium color="primary">Salvar</v-btn>
-    <span class="material-icons">face</span>
-  </div>
+  <div class="ml-16">
+      <v-row>
+        <v-col cols="2" style=" background:#FF3565"></v-col>
+         <v-col cols="6" style=" background:#3A1C71">
+        <v-btn elevation="0" large color="primary" class="font-weight-bold">
+            <v-icon left light>add</v-icon>Novo sonho</v-btn>
+        </v-col>
+      </v-row>
+
+    </div>
 </template>
 
 <script>
 export default {
+
   name: 'HelloWorld',
   data() {
     return {
